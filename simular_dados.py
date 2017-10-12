a0859ae9c7adfa4612a2b8ca869697743e92c3b8
frcleiton <cleitonrferreira@gmail.com>
2017-10-11 21:48:20 -0300

    add cfg

---
+++ simular_dados.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import paho.mqtt.client as mqtt
+import json
+import random
+import ConfigParser
+from datetime import date, datetime, timedelta
+
+config = ConfigParser.ConfigParser()
+config.read('sensor.cfg')
+
+#Servidor Local
+MQTT_ADDRESS    = config.get('config','plataforma')
+MQTT_PORT       = config.get('config','porta')
+MQTT_TIMEOUT    = 60
+SENSOR_ID       = config.get('config','dispositivo')
+DEBUG           = config.get('config','mododebug')
+
+client = mqtt.Client()
+client.username_pw_set('ticleiton', 'ti@cleiton')
+client.connect(MQTT_ADDRESS, MQTT_PORT, MQTT_TIMEOUT)
+
+def publish_value(_temperature, _humidity, _str_time):
+
+	temp_topic = SENSOR_ID+'/temperature'
+	humi_topic = SENSOR_ID+'/humidity'
+
+	if _temperature is not None:
+		send_msg = {'t': _str_time,
+					'mu': 'C',
+					'value': _temperature}
+		result, mid = client.publish(temp_topic, payload=json.dumps(send_msg), qos=1, retain=True )
+		print "%s - %s" % (temp_topic, send_msg)
+
+	if _humidity is not None:
+		send_msg = {'t': _str_time,
+					'mu': 'RH',
+					'value': _humidity}
+		result, mid = client.publish(humi_topic, payload=json.dumps(send_msg), qos=1, retain=True )
+		print "%s - %s" % (humi_topic, send_msg)
+
+
+def read_sensor():
+
+	data_hora_gerada = datetime.now() - timedelta(days=10)
+		
+	for i in range(1000):
+		str_time = data_hora_gerada.strftime("%d-%m-%Y %H:%M:%S")
+		temperature = round(random.randint(0, 40)+random.random(),2)
+		humidity = round(random.randint(0, 100)+random.random(),2)
+		publish_value(round(temperature,2), round(humidity,0), str_time)
+		data_hora_gerada = data_hora_gerada + timedelta(minutes=1)
+		
+
+if __name__ == '__main__':
+	read_sensor()
